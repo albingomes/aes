0016316e9d2e68291b20d6e8bea39fc34c700e5f
albingomes <albin.gomes@gmail.com>
2022-03-05 17:20:41 -0700

    key expansion rtl update

--- top/rtl/aes_top.sv
+++ top/rtl/aes_top.sv
@@ -8,11 +8,14 @@
 // Version      :
 //===================================================================================
 
-module aes_top (
+module aes_top #(
+  parameter int key_size = 128 // default = 128, choices: 128, 192, 256
+)(
   // Input
   input         clk,
   input         reset_n,
-  input [127:0] key_128,
+  input         enable,
+  input [255:0] key,        // shared for all key size use, for example bits [127:0] used for key size 128 bits
   input [127:0] plaintext,
   // Output
   output [127:0] ciphertext
@@ -22,18 +25,32 @@ module aes_top (
 // Nets and Regs
 //-----------------------------------------------------------------------------------
 
-logic [127:0] key_out;
+logic [255:0] key_out; // shared for all key sizes
 logic [4:0]   round;
 
 
 //-----------------------------------------------------------------------------------
-// Instantations
+// Instantiations
 //-----------------------------------------------------------------------------------
 
-key_exp_top key_exp_top_0 (
-  .clk        (clk),
-  .reset_n    (reset_n),
-  .round      (round),
-  .key        (key_128),
-  .key_out    (key_out)
-);
+generate
+  case (key_size)
+    128: begin
+        key_exp_128_0 (
+          .clk        (clk), 
+          .reset_n    (reset_n),
+          .enable     (enable),
+          .round      (round),
+          .key        (key[127:0]),
+          .key_out    (key_out[127:0])
+        );
+      end 
+    192:
+
+    256: 
+
+  endcase 
+endgenerate 
+
+
+endmodule
