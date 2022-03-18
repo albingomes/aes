996e0b969b339421cf89e2724a3bec1f7506b1cc
albingomes <albin.gomes38@gmail.com>
2022-03-17 20:18:39 -0600

    updated s_box.sv

--- key_exp/rtl/s_box.sv
+++ key_exp/rtl/s_box.sv
@@ -21,6 +21,13 @@ module s_box (
 // Nets, Regs and states
 //-----------------------------------------------------------------------------------
 
+logic      [2:0] present_state, next_state;
+// FSM encoding: hard encoded
+localparam s0  = 3'b001;
+localparam s1  = 3'b010;
+localparam s2  = 3'b100;
+
+
 //-----------------------------------------------------------------------------------
 // Instantiations
 //-----------------------------------------------------------------------------------
@@ -33,6 +40,46 @@ module s_box (
 // Process
 //-----------------------------------------------------------------------------------
 
+//--------------
+//State Machine
+//--------------
+
+//async next state logic
+always_comb begin
+  case(present_state) 
+     s0:
+     begin
+     
+     
+     end 
+     default:
+     begin
+     
+     end
+  endcase
+end 
+
+//sync signal assignment
+always_ff(posedge clk, negedge reset_n) begin
+  if(reset_n == 0) begin
+  
+  
+  end
+  else begin
+    present_state <= next_state;
+    case(present_state) 
+      s0:
+      begin
+    
+      end 
+      default:
+      begin
+    
+    
+      end
+    endcase
+  end
+end 
 
 
 endmodule
